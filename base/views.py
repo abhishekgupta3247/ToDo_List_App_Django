a58d588caaf1e758d3a638767615d81d363233e5
abhishekgupta3247 <abhishekgupta3247@gmail.com>
2023-10-15 17:36:16 +0530

    http://127.0.0.1:5000/task/1/

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# Commented out this bcoz we'll be using class based view
+## Commented out this bcoz we'll be using class based view
 
 # from django.http import HttpResponse
 
@@ -10,10 +10,17 @@ from django.shortcuts import render
 #     return HttpResponse('To Do List')
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Task
 
+
+# looks for task_list.html
 class TaskList(ListView):
     model = Task
     # setting context object name for task_list.html
     context_object_name = 'tasks'
+
+# looks for task_detail.html
+class TaskDetail(DetailView):
+    model = Task
